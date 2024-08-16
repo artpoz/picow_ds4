3e79fa5f68bcd5cc20659cc97813aaa5e268fabb
Artur Poznański <apoznan5@gmail.com>
2024-08-16 09:06:05 +0200

    modified for Kitronik Autonomous Robotics Platform

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2023 Brian Starkey <stark3y@gmail.com>
+// modified by Artur Poznanski <apoznan5@gmail.com> 
+// for Kitronik Autonomous Robotics Platform
 
 #include <stdio.h>
 #include <string.h>
@@ -11,10 +13,8 @@
 
 #include "bt_hid.h"
 
-// These magic values are just taken from M0o+, not calibrated for
-// the Tiny chassis.
-#define PWM_MIN 80
-#define PWM_MAX (PWM_MIN + 127)
+#define PWM_MIN 0
+#define PWM_MAX 100 
 
 static inline int8_t clamp8(int16_t value) {
         if (value > 127) {
@@ -29,6 +29,7 @@ static inline int8_t clamp8(int16_t value) {
 struct slice {
 	unsigned int slice_num;
 	unsigned int pwm_min;
+	unsigned int pwm_max;
 };
 
 struct chassis {
@@ -91,7 +92,6 @@ void chassis_set_raw(struct chassis *chassis, int8_t left, int8_t right)
 
 void chassis_set(struct chassis *chassis, int8_t linear, int8_t rot)
 {
-	// Positive rotation == CCW == right goes faster
 
 	if (linear < -127) {
 		linear = -127;
@@ -103,51 +103,42 @@ void chassis_set(struct chassis *chassis, int8_t linear, int8_t rot)
 
 	int l = linear - rot;
 	int r = linear + rot;
-	int adj = 0;
 
-	if (l > 127) {
-		adj = l - 127;
-	} else if (l < -127) {
-		adj = l + 127;
-	}else if (r > 127) {
-		adj = r - 127;
-	} else if (r < -127) {
-		adj = r + 127;
-	}
-
-	l -= adj;
-	r -= adj;
-
-	// FIXME: Motor directions should be a parameter
 	r = -r;
-
+    
 	chassis_set_raw(chassis, l, r);
 }
 
 void main(void) {
 	stdio_init_all();
 
-	sleep_ms(1000);
+	sleep_ms(5000);
 	printf("Hello\n");
 
 	multicore_launch_core1(bt_main);
-	// Wait for init (should do a handshake with the fifo here?)
-	sleep_ms(1000);
+	sleep_ms(5000);
 
-	struct chassis chassis = { 0 };
-	chassis_init(&chassis, 6, 8);
+	struct chassis chassis1 = { 0 };
+    	struct chassis chassis2 = { 0 };
+
+    	chassis_init(&chassis1, 20, 6);
+    	chassis_init(&chassis2, 19, 7);
 
 	struct bt_hid_state state;
 	for ( ;; ) {
 		sleep_ms(20);
 		bt_hid_get_latest(&state);
-		printf("buttons: %04x, l: %d,%d, r: %d,%d, l2,r2: %d,%d hat: %d\n",
-				state.buttons, state.lx, state.ly, state.rx, state.ry,
-				state.l2, state.r2, state.hat);
+		
+		float speed_scale1 = 0.1;
+        	float speed_scale2 = 0.05;
+		int8_t linear = clamp8(-(state.rx - 128) * speed_scale2);
+		int8_t rot = clamp8(-(state.ly - 128) * speed_scale1);
 
-		float speed_scale = 1.0;
-		int8_t linear = clamp8(-(state.ly - 128) * speed_scale);
-		int8_t rot = clamp8(-(state.rx - 128));
-		chassis_set(&chassis, linear, rot);
+		//printf("buttons: %04x, l: %d,%d, r: %d,%d, l2,r2: %d,%d hat: %d linear: %d, rot: %d\n",
+		//		state.buttons, state.lx, state.ly, state.rx, state.ry,
+		//		state.l2, state.r2, state.hat, linear, rot);
+				
+		chassis_set(&chassis1, linear, rot);
+        	chassis_set(&chassis2, linear, rot);
 	}
 }
